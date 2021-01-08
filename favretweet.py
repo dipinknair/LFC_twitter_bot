f94979555d245aeb73ea89553528f95701c13832
dipinknair <dipinknair619@gmail.com>
2021-01-07 19:42:37 -0500

    Add files via upload

---
+++ favretweet.py
@@ -0,0 +1,65 @@
+import tweepy
+import logging
+import os
+from os import environ
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+def create_api():
+    consumer_key = environ['consumer_key']
+    consumer_secret = environ['consumer_secret']
+    access_token = environ['access_token']
+    access_token_secret = environ['access_token_secret']
+
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
+
+api = create_api()
+print("Authenticated as: %s" % api.me().screen_name)
+
+class FavRetweetListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        logger.info(f"Processing tweet id {tweet.id}")
+        if tweet.in_reply_to_status_id is not None or \
+            tweet.user.id == self.me.id:
+            # This tweet is a reply or I'm its author so, ignore it
+            return
+        if not tweet.favorited:
+            # Mark it as Liked, since we have not done it yet
+            try:
+                tweet.favorite()
+            except Exception as e:
+                logger.error("Error on fav", exc_info=True)
+        if not tweet.retweeted:
+            # Retweet, since we have not retweeted it yet
+            try:
+                tweet.retweet()
+            except Exception as e:
+                logger.error("Error on fav and retweet", exc_info=True)
+
+    def on_error(self, status):
+        logger.error(status)
+
+def main(keywords):
+    api = create_api()
+    tweets_listener = FavRetweetListener(api)
+    stream = tweepy.Stream(api.auth, tweets_listener)
+    stream.filter(track=keywords, languages=["en"])
+
+if __name__ == "__main__":
+    main(["#LFC"])
